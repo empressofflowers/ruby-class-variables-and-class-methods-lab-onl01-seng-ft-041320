6c99d87ab4ff62e776b962499c38545986b08874
Devone Austin <devone.austin@gmail.com>
2020-04-23 02:02:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,8 +32,8 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count =+ 1
-      else genre_count = 1
+        genre_count[genre] =+ 1
+      else genre_count[genre] = 1
       end
     end
     genre_count
