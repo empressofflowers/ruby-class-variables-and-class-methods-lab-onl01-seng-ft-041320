c8f7fed16f52ae75cf385df02f1c2d98f418a69a
Devone Austin <devone.austin@gmail.com>
2020-04-23 01:47:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,8 @@ class Song
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist]! +1 
+      if artist_count[artist]
+        +1 
       end
     end
     artist_count
